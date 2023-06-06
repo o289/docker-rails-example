f4cdea218c327f82cc1a75e71b757f0e61c82f11
o289 <nanyouda11@gmail.com>
2023-06-06 20:45:50 +0900

    Second commit.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+      sessions: 'admin/sessions'
+  }
+  devise_for :customers, controllers: {
+      sessions: 'customer/sessions',
+      registrations: 'customer/registrations'
+    }
   root to: 'pages#home'
 
   get '/up/', to: 'up#index', as: :up
